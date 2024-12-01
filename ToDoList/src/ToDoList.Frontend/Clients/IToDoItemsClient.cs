1c275217598bbe3f147f900a8db8550d87f24d94
Stark Roman <stark@spsostrov.cz>
2024-12-01 15:59:15 +0100

    Úkol 10.1 Uprava FrontEndu

--- ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
@@ -1,7 +1,11 @@
 namespace ToDoList.Frontend.Clients;
 using ToDoList.Frontend.Views;
+
 public interface IToDoItemsClient
 {
-    // doplnit Task<>
+    public Task CreateItemAsync(ToDoItemView itemView);
+    public Task<ToDoItemView?> ReadItemByIdAsync(int itemId);
     public Task<List<ToDoItemView>> ReadItemsAsync();
+    public Task DeleteItemAsync(ToDoItemView itemView);
+    public Task UpdateItemAsync(ToDoItemView itemView);
 }
