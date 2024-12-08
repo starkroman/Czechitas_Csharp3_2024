14196e03ac3dd18b9b48546128e2d8bf151061fd
Stark Roman <stark@spsostrov.cz>
2024-12-08 14:36:24 +0100

    Dokončení projektu - přidáno CreateToDoItem.razor - funkční

--- ToDoList/src/ToDoList.Frontend/Views/ToDoItemView.cs
+++ ToDoList/src/ToDoList.Frontend/Views/ToDoItemView.cs
@@ -1,9 +1,31 @@
 namespace ToDoList.Frontend.Views;
+
+using System.ComponentModel.DataAnnotations;
+
 public class ToDoItemView
 {
+    /*
     public int ToDoItemId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public bool IsCompleted { get; set; }
     public string? Category { get; set;}
+    */
+
+
+    public int ToDoItemId { get; set; }
+
+    [Required(ErrorMessage = "Name is mandatory.")]
+    [Length(3, 50)]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "Description is mandatory.")]
+    [StringLength(250)]
+    public required string Description { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    [Required(ErrorMessage = "Catefory is mandatory.")]
+    [StringLength(350)]
+    public required string? Category { get; set; }
 }
